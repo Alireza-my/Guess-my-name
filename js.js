e2abcd547edf03345ee6495cf6a0b4b107c17375
AlirezaMahmoodi <alireza.mahmoodi.amai@gmail.com>
2022-11-06 20:41:55 +0330

    select element

---
+++ js.js
@@ -0,0 +1,9 @@
+"strict mode";
+
+const check = document.querySelector(".check");
+const message = document.querySelector(".message");
+const score = document.querySelector(".label-score");
+const guess = document.querySelector(".guess");
+const secretNumber = document.querySelector(".number");
+const again = document.querySelector(".again");
+const highscore = document.querySelector(".label-highscore");
