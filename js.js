85c41ff7b11aa0be14f27e02a1fc871037c7c1c7
AlirezaMahmoodi <alireza.mahmoodi.amai@gmail.com>
2022-11-06 21:19:51 +0330

    logic part of js except highscore

--- js.js
+++ js.js
@@ -2,8 +2,42 @@
 
 const check = document.querySelector(".check");
 const message = document.querySelector(".message");
-const score = document.querySelector(".label-score");
+const score = document.querySelector(".score");
 const guess = document.querySelector(".guess");
 const secretNumber = document.querySelector(".number");
 const again = document.querySelector(".again");
 const highscore = document.querySelector(".label-highscore");
+
+// logic
+
+let number = Math.trunc(Math.random() * 20 + 1);
+// secretNumber.textContent = number;
+let yourScore = 20;
+
+check.addEventListener("click", cac);
+function cac() {
+  let myGs = guess.value;
+  if (!myGs) {
+    message.textContent = "No number❗";
+  } else if (myGs == number) {
+    message.textContent = "Correct number🏆";
+  } else if (myGs > number) {
+    if (yourScore > 1) {
+      message.textContent = "To high📈";
+      yourScore--;
+      score.textContent = yourScore;
+    } else {
+      message.textContent = "You lost the game!💣";
+      score.textContent = 0;
+    }
+  } else if (myGs < number) {
+    if (yourScore > 1) {
+      message.textContent = "To low📉";
+      yourScore--;
+      score.textContent = yourScore;
+    } else {
+      message.textContent = "You lost the game!💣";
+      score.textContent = 0;
+    }
+  }
+}
